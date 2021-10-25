dbc0dc1786303c85772fbc90735f3c8431c549cb
Shantanu Chakraborty <chakrabortyss19.comp@coep.ac.in>
2021-10-25 07:21:39 +0000

    Update http.py

--- http.py
+++ http.py
@@ -6,19 +6,14 @@ import datetime
 from datetime import date
 
 
-
-
- 
-class TCPServer:
+class makesocket:
     
 
-    def __init__(self, host='127.0.0.1', port=8888):
+    def __init__(self, host='127.0.0.1', port=1234):
         self.host = host
         self.port = port
 
     def start(self):
-        
-
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((self.host, self.port))
@@ -29,9 +24,6 @@ class TCPServer:
         while True:
             conn, addr = s.accept()
             print("Connected by", addr)
-            
-            # For the sake of this tutorial, 
-            # we're reading just the first 1024 bytes sent by the client.
             data = conn.recv(2048) 
 
             response = data
@@ -42,11 +34,9 @@ class TCPServer:
     
 
 
-class HTTPServer(TCPServer):
-    """The actual HTTP server class."""
+class HTTPServer(makesocket)
 
     headers = {
-       # 'Server': 'CrudeServer',
         'Content-Type': 'text/html',
         'Connection type' : 'keep-alive'
     }
@@ -68,11 +58,9 @@ class HTTPServer(TCPServer):
     def handle_request(self, data):
         """Handles incoming requests"""
 
-        request = HTTPRequest(data) # Get a parsed HTTP request
+        request = HTTPRequest(data) #
 
         try:
-            # Call the corresponding handler method for the current 
-            # request's method
             handler = getattr(self, 'handle_%s' % request.method)
         except AttributeError:
             handler = self.HTTP_501_handler
@@ -81,18 +69,13 @@ class HTTPServer(TCPServer):
         return response
 
     def response_line(self, status_code):
-        """Returns response line (as bytes)"""
         reason = self.status_codes[status_code]
         response_line = 'HTTP/1.1 %s %s\r\n' % (status_code, reason)
 
-        return response_line.encode() # convert from str to bytes
+        return response_line.encode() 
 
     def response_headers(self, extra_headers=None):
-        """Returns headers (as bytes).
-        The `extra_headers` can be a dict for sending 
-        extra headers with the current response
-        """
-        headers_copy = self.headers.copy() # make a local copy of headers
+        headers_copy = self.headers.copy() 
 
         if extra_headers:
             headers_copy.update(extra_headers)
@@ -105,11 +88,9 @@ class HTTPServer(TCPServer):
         return headers.encode() # convert str to bytes
 
     def handle_OPTIONS(self, request):
-        """Handler for OPTIONS HTTP method"""
-
         response_line = self.response_line(200)
 
-        extra_headers = {'Allow': 'OPTIONS, GET'}
+        extra_headers = {'Allow': 'OPTIONS, GET , POST , PUT, DELETE'}
         response_headers = self.response_headers(extra_headers)
 
         blank_line = b'\r\n'
@@ -117,16 +98,12 @@ class HTTPServer(TCPServer):
         return b''.join([response_line, response_headers, blank_line])
 
     def handle_GET(self, request):
-        """Handler for GET HTTP method"""
 
-        path = request.uri.strip('/') # remove slash from URI
-
+        path = request.uri.strip('/') 
         if not path:
-            # If path is empty, that means user is at the homepage
-            # so just serve index.html
             path = 'index.html'
 
-        if os.path.exists(path) and not os.path.isdir(path): # don't serve directories
+        if os.path.exists(path) and not os.path.isdir(path): 
             response_line = self.response_line(200)
             content_type  = mimetypes.guess_type(path)[0] or 'text/html'
             
